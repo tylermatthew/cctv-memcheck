725618ba89c20b1dc1f3873cffdcfc092eb7fc96
tylermatthew <130069623+tylermatthew@users.noreply.github.com>
2023-04-10 12:05:48 -0400

    Create restart_cctv-viewer.sh
    Bash script to check on total memory usage, restart cctv-viewer at a certain high-limit threshold, restart it after a certain low-limit threshold, and log it's work. It also checks to ensure cctv-viewer is running at all, launches it if not, and logs its work. It also works to make sure the log file doesn't exceed a certain size.

---
+++ restart_cctv-viewer.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Set the memory threshold for stopping the service (in kilobytes)
+threshold=2500000 # 2.5GB
+
+# Set the memory threshold for restarting the service (in kilobytes)
+restart_threshold=1000000 # 1GB
+
+# Set the name of the service to monitor
+service_name="cctv-viewer"
+
+# Set the path to the log file
+log_file="/var/log/restart_cctv-viewer.log"
+
+# Define the highest number of lines the log file should be allowed to reach
+logfilesize_threshold=5000
+
+# Define the number of lines to retain in the log file during its re-write
+logfilesize_retain=500
+
+# Check if the log file exists
+if [ ! -f "$log_file" ]; then
+
+    # If it doesn't exist, create it
+    touch "$log_file"
+fi
+
+# Check if the log file has reached the threshold
+if [ $(wc -l < "$log_file") -gt $logfilesize_threshold ]; then
+
+    # If it has, remove all but the last $logfilesize_retain entries
+    tail -n $logfilesize_retain "$log_file" > "$log_file.tmp"
+    mv "$log_file.tmp" "$log_file"
+fi
+
+# Start an infinite loop
+while true; do
+
+    # Get the current memory usage
+    mem_usage=$(free | awk '/Mem/{print $3}')
+    
+    # Check if the memory usage is above the threshold
+    if [ $mem_usage -gt $threshold ]; then
+	
+        # Log that we're stopping the service due to high memory usage
+        echo "$(date): Stopping $service_name due to high memory usage" >> $log_file
+
+        # Kill the service and log any output or errors
+        (killall $service_name 2>&1 | sed "s/^/$(date): snap reply - /" >> $log_file)
+
+        # Wait for the memory usage to drop below the restart threshold
+        while [ $mem_usage -gt $restart_threshold ]; do
+		
+            # Sleep for 2 seconds before checking again
+            sleep 2
+			
+            # Get the current memory usage again
+            mem_usage=$(free | awk '/Mem/{print $3}')
+        done
+
+        # Log that we're starting the service after memory usage dropped below threshold
+        echo "$(date): Starting $service_name after memory usage dropped below ($restart_threshold/1000000) Gb" >> $log_file
+
+        # Start the service in fullscreen and log any output or errors
+        (snap run $service_name -f 2>&1 | sed "s/^/$(date): snap reply - /" >> $log_file)
+    fi
+
+        # wait 10 seconds to give cctv-viewer time to start
+        sleep 10
+
+        # Check if the cctv-viewer program is running
+    if ! pgrep -x "$service_name" > /dev/null; then
+
+        # Start the cctv-viewer program in fullscreen mode
+        snap run $service_name -f &
+
+        # Write a log entry to the cctv-viewer.log file
+        echo "$(date): Error - Started $service_name in fullscreen mode after finding it not running" >> $log_file
+    fi
+    
+    # Sleep for 1 minute before checking again
+    sleep 60
+done
