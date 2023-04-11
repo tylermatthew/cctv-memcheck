4f005b00480de7ea576dc36963c92cc0694c0d33
tylermatthew <130069623+tylermatthew@users.noreply.github.com>
2023-04-10 23:51:05 -0400

    Update for rc.local compatibility
    attempting to make this thing run the snap commands as user instead of root, since snap fails when run as root.

--- restart_cctv-viewer.sh
+++ restart_cctv-viewer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# first, we need to set the username for whatever machine this runs on, since snap's annoyingly fail to run when started by root
+
+usr=(ls -l /home | awk '{print $3}' | tail -n +2) # this is a pretty roundabout way to do it, but hey I avoided sed, so there's that.
 
 # Set the memory threshold for stopping the service (in kilobytes)
 threshold=2500000 # 2.5GB
@@ -25,10 +29,13 @@ if [ ! -f "$log_file" ]; then
     touch "$log_file"
 fi
 
+# lets make sure the user owns the log file
+chown "$usr":"$usr" "$log_file"
+
 # Check if the log file has reached the threshold
 if [ $(wc -l < "$log_file") -gt $logfilesize_threshold ]; then
 
-    # If it has, remove all but the last $logfilesize_retain entries
+    # If it has, remove all but the last number of entries specified
     tail -n $logfilesize_retain "$log_file" > "$log_file.tmp"
     mv "$log_file.tmp" "$log_file"
 fi
@@ -57,12 +64,12 @@ while true; do
             # Get the current memory usage again
             mem_usage=$(free | awk '/Mem/{print $3}')
         done
-
-        # Log that we're starting the service in kiosk mode after memory usage dropped below threshold
-        echo "$(date): Starting $service_name in kiosk mode after memory usage dropped below ($restart_threshold/1000000) Gb" >> $log_file
-
-        # Start the service in fullscreen and log any output or errors
-        (snap run $service_name -k 2>&1 | sed "s/^/$(date): snap reply - /" >> $log_file)
+		
+		# Start the service in fullscreen and log any output or errors
+		(su -c 'snap run "$service_name" -k 2>&1' "$usr" | sed "s/^/$(date): snap reply - /" >> "$log_file" &)
+			
+			# Log that we're starting the service in kiosk mode after memory usage dropped below threshold
+        echo "$(date): Starting $service_name in kiosk mode after memory usage dropped below ($restart_threshold/1000000) Gb" >> "$log_file"
     fi
 
         # wait 10 seconds to give cctv-viewer time to start
@@ -72,10 +79,10 @@ while true; do
     if ! pgrep -x "$service_name" > /dev/null; then
 
         # Start the cctv-viewer program in Kiosk mode
-        snap run $service_name -k &
+        (su -c 'snap run "$service_name" -k 2>&1' "$usr" | sed "s/^/$(date): snap reply - /" >> "$log_file" &)
 
         # Write a log entry to the cctv-viewer.log file
-        echo "$(date): Error - Started $service_name in fullscreen mode after finding it not running" >> $log_file
+        echo "$(date): Error - Started $service_name in fullscreen mode after finding it not running" >> "$log_file"
     fi
     
     # Sleep for 1 minute before checking again
