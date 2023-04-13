58a83cf736a69cc5c000a148c46f809da83b98e4
tylermatthew <130069623+tylermatthew@users.noreply.github.com>
2023-04-13 01:42:11 -0400

    Update rcv_install.sh
    general fixes

--- rcv_install.sh
+++ rcv_install.sh
@@ -4,31 +4,35 @@
 # and appropriately set up the viewing machine so that cctv-viewer is displayed at
 # least 99% of the time. 
 
-# Lets set some functions
+usr=$(ls -l /home | awk '{print $3}' | tail -n +2)
 script_name="restart_cctv-viewer"
 
-raw_github_code_URL="https://raw.githubusercontent.com/tylermatthew/cctv-viewer-memleak-fix/main/restart_cctv-viewer.sh"
-
-# Get code from $raw_github_code_URL and write it as a .sh file with $script_name to /usr/local/bin/
-curl -o /usr/local/bin/"$script_name".sh "$raw_github_code_URL"
+echo "getting $script_name..."
+wget /usr/local/bin/restart_cctv-viewer.sh https://raw.githubusercontent.com/tylermatthew/cctv-viewer-memleak-fix/main/restart_cctv-viewer.sh; wait
 
 # Make it executable
-chmod +x /usr/local/bin/"$script_name".sh
-
-# Ensure it will run with permissions to start and kill services, and write new directories and files
-chown root:root /usr/local/bin/"$script_name".sh
-chmod u+s /usr/local/bin/"$script_name".sh
+echo "making it x..."
+chmod +x /usr/local/bin/"$script_name".sh; sleep 1
 
 # Create an /etc/rc.local file if none exists, and add $script_name there so that it starts at system boot
 if [ ! -f /etc/rc.local ]; then
-    echo -en '#! /bin/sh -e \n # \n # rc.local - executed at the end of each multiuser runlevel \n # \n # Make sure that the script will "exit 0" on success or any other \n # value on error. \n \n \n exit 0' | sed 's/ //' >> /etc/rc.local
-   
-    chmod +x /etc/rc.local
+    echo "creating rc.local..."
+    echo -e '#!  /bin/sh -e \n # \n # rc.local - executed at the end of each multiuser runlevel \n # \n # Make sure that the script will "exit 0" on success or any other \n # value on error. \n\n /usr/local/bin/'"$script_name"'.sh & \n\n exit 0' | sed 's/ //' >>
+    chmod +x /etc/rc.local; sleep 1
 fi
-sed -i '$i/usr/local/bin/'"$script_name"'.sh &' /etc/rc.local
 
 # Create the log file 
+echo "creating log file and giving it to $usr..."
 touch /var/log/"$script_name".log
+chown "$usr":"$usr" /var/log/"$script_name".log
+
+echo "verifying..." 
+
+ls /usr/local/bin/ | grep restart >/dev/null || echo "ERROR: No rcv in user's bin folder!" && echo "$(date) ERROR: No rcv in user's bin folder!" >> /var/log/$script_name.log
+ls /var/log/ | grep restart >/dev/null || echo "ERROR: No $script_name log file!" && echo "$(date) ERROR: No $script_name log file!" >> /var/log/$script_name.log
+ls /etc | grep rc.local >/dev/null || echo "ERROR: No rc.local file!" && echo "$(date) ERROR: No rc.local file!" >> /var/log/$script_name.log
+cat /etc/rc.local | grep $script_name >/dev/null || echo "ERROR: $script_name not found in the rc.local file!" && echo "$(date) ERROR: $script_name not found in the rc.local file!" >> /var/log/$script_name.log
+log_own=$(ls -all /var/log/ | grep cctv | grep $usr | awk '{print $3}'); [[ $usr = $log_own ]] || echo "ERROR: $script_name log isnt owned by $usr!" && echo "$(date) ERROR: $script_name log isnt owned by $usr!" >> /var/log/$script_name.log
 
 # Run the restart script
 /usr/local/bin/"$script_name".sh
@@ -37,5 +41,6 @@ touch /var/log/"$script_name".log
 if [ $? -eq 0 ]; then
     echo "Done!"
 else
-    echo "An error occurred while running the script. Please check the log file for more information."
+    echo "An error occurred while running the script. Please check the log file for more information."; exit 1
 fi
+exit 0
