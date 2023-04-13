406e2632bd41f6f61901ad393de376e2d7a889ec
tylermatthew <130069623+tylermatthew@users.noreply.github.com>
2023-04-13 03:07:19 -0400

    rcv script v0.1.1
    compacted, upgraded, and actually working?? lets go!

--- restart_cctv-viewer.sh
+++ restart_cctv-viewer.sh
@@ -1,103 +1,71 @@
 #!/bin/bash
+#
+# This is the
+#
+#	  \  |      |   |_)       |    		License: GNU General Public License v3.0
+#	   \ |  _ \ __| | | __ \  |  / 		Version: 0.1.1
+#	 |\  |  __/ |   | | |   |   <  		Author: Tyler Johnson
+#	_| \_|\___|\__|_|_|_|  _|_|\_\		only tested on Ubuntu 22.04
+#
+# 		  simple loop trying to keep 
+# 				cctv-viewer 
+#	displaying camera feeds, no matter what! 
+# 		assumed to be unattended
 
-# first, we need to set the username for whatever machine this runs on, since snaps annoyingly fail to run when started by root
+# first, we need to set the username for whatever machine this runs on, 
+# since snaps annoyingly fail to run when started by root
+# this is a pretty roundabout way to do it, but hey I avoided sed, so there's that.
 
-usr=$(ls -l /home | awk '{print $3}' | tail -n +2) # this is a pretty roundabout way to do it, but hey I avoided sed, so there's that.
-
-# Set the memory threshold for stopping the service (in kilobytes)
-threshold=2500000 # 2.5GB
-
-# Set the memory threshold for restarting the service (in kilobytes)
-restart_threshold=1000000 # 1GB
-
-# Set the name of the service to monitor
-service_name="cctv-viewer"
-
-# Set the path to the log file
-log_file="/var/log/restart_cctv-viewer.log"
-
-# Define the highest number of lines the log file should be allowed to reach
-logfilesize_threshold=5000
-
-# Define the number of lines to retain in the log file during its re-write
-logfilesize_retain=500
+usr=$(ls -l /home | awk '{print $3}' | tail -n +2) 
+threshold=2500000 # Set the memory threshold for stopping the service (in kilobytes)
+restart_threshold=1000000 # Set the memory threshold for restarting the service (in kilobytes)
+service_name="cctv-viewer" # Set the name of the service to monitor
+log_file="/var/log/restart_cctv-viewer.log" # Set the path to the log file
+logfilesize_threshold=5000 # Define the highest number of lines the log file should be allowed to reach
+logfilesize_retain=500 # Define the number of lines to retain in the log file during its re-write
 
 # before we get into our loop, let's do some housekeeping
 # first off, lets log the script starting up
 
-# Check if the log file exists
+# Check if the log file exists, of not make it and own it
 if [ ! -f "$log_file" ]; then
-
-    # If it doesn't exist, create it
     touch "$log_file"
-	
-	# make sure the user owns the log file
 	chown "$usr":"$usr" "$log_file"
 fi
 
-if ps aux | grep -v grep | grep -q '/etc/rc.local'; then
-
-    # rc.local is running
- 	echo 'Starting up restart_cctv-viewer.sh at boot time' > "$log_file"
-else
-
-    # rc.local is not running
-	echo 'Starting up restart_cctv-viewer.sh when rc.local is not running' > "$log_file"
-fi
-
 # Check if the log file has reached the threshold
+# If it has, remove all but the last number of entries specified
 if [ $(wc -l < "$log_file") -gt $logfilesize_threshold ]; then
-
-    # If it has, remove all but the last number of entries specified
-    tail -n $logfilesize_retain "$log_file" > "$log_file.tmp"
+	tail -n $logfilesize_retain "$log_file" > "$log_file.tmp"
     mv "$log_file.tmp" "$log_file"
 fi
 
 # Start our primary infinite loop of checking, stopping, and starting
 while true; do
-
-    # Get the current memory usage
-    mem_usage=$(free | awk '/Mem/{print $3}')
-    
-    # Check if the memory usage is above the threshold
-    if [ $mem_usage -gt $threshold ]; then
+	mem_usage=$(free | awk '/Mem/{print $3}') # Get the current memory usage
+	if [ $mem_usage -gt $threshold ]; then # Check if the memory usage is above the threshold
 	
         # Log that we're stopping the service due to high memory usage
         echo "$(date): Stopping $service_name due to memory exceeding $(expr $threshold \/ 1000000) Gb" >> "$log_file"
-
-        # Kill the service and log any output or errors
-        (killall $service_name 2>&1 | sed "s/^/$(date): /" >> $log_file)
+		(killall $service_name 2>&1 | sed "s/^/$(date): /" >> $log_file) # Kill the service and log any output or errors
 
         # Wait for the memory usage to drop below the restart threshold
-        while [ $mem_usage -gt $restart_threshold ]; do
-		
-            # Sleep for 2 seconds before checking again
+        while [ $mem_usage -gt $restart_threshold ]; do 
             sleep 2
-			
-            # Get the current memory usage again
-            mem_usage=$(free | awk '/Mem/{print $3}')
+			mem_usage=$(free | awk '/Mem/{print $3}')
         done
 		
 		# Start the service in fullscreen and log any output or errors
-		(su $usr -c "$service_name -k" 2>&1 | sed "s/^/$(date): /" >> "$log_file" &)
-			
-			# Log that we're starting the service in kiosk mode after memory usage dropped below threshold
-        echo "$(date): Starting $service_name in kiosk mode after memory usage dropped below $(expr $restart_threshold \/ 1000000) Gb" >> "$log_file"
+		su -c "$service_name" "$usr" 2>&1 | sed "s/^/$(date): /" >> "$log_file" &
+		echo "$(date): Starting $service_name in kiosk mode after memory usage dropped below $(expr $restart_threshold \/ 1000000) Gb" >> "$log_file"
     fi
+	sleep 10
 
-        # wait 10 seconds to give cctv-viewer time to start
-        sleep 10
-
-        # Check if the cctv-viewer program is running
+        # Check if the cctv-viewer program is running, and Start the cctv-viewer program in Kiosk mode
     if ! pgrep -x "$service_name" > /dev/null; then
-
-        # Start the cctv-viewer program in Kiosk mode
-        (su $usr -c "$service_name -k" 2>&1 | sed "s/^/$(date): /" >> "$log_file" &)
-
-        # Write a log entry to the cctv-viewer.log file
-        echo "$(date): Error - Started $service_name in kiosk mode after finding it not running" >> "$log_file"
+		su -c "$service_name" "$usr" 2>&1 | sed "s/^/$(date): /" >> "$log_file" &
+		echo "$(date): Error - Started $service_name in kiosk mode after finding it not running" >> "$log_file"
     fi
-    
-    # Sleep for 1 minute before checking again
-    sleep 60
+	sleep 60 # Sleep for 1 minute before checking again
 done
+exit 0
